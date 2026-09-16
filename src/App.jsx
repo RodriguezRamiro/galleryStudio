174253458f71a2c0f4d9d2469ba7b9614fd80735
Ramiro Rodriguez <rodriguezcodesolutions@gmail.com>
2026-09-16 13:45:41 -0400

    imported the header and updated app component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 /* galleryStudio/src/App.jsx */
 
+import { useState } from 'react'
+import Header from './components/Header.jsx'
+
 function App() {
-    return (
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const [isDark, setIsDark] = useState(false)
+
+  return (
+    <>
+      <Header
+        isMenuOpen={isMenuOpen}
+        setIsMenuOpen={setIsMenuOpen}
+        isDark={isDark}
+        setIsDark={setIsDark}
+      />
+
       <main>
         <h1>Craftmancer Studios</h1>
         <p>Digital Exhibition</p>
       </main>
-    )
-  }
+    </>
+  )
+}
 
-  export default App
+export default App
+
